5f86410f0b28d2819e0fc69d7b4923c84c8e0c12
Daniel Bobrov <bobrovdanel@gmail.com>
2024-07-22 14:11:05 +0300

    hot tix

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-python generate_dataset.py
-python generate_labels.py
-python generate_configs.py
-python train_model.py
+python3 generate_dataset.py
+echo "dataset generated"
+python3 generate_labels.py
+echo "labels generated"
+python3 generate_configs.py
+echo "configs generated"
+python3 train_model.py
+echo "model trained"
